40d45430c0dc8247123841982b862dfaa983b9a9
tk22kalal4 <tarangkalal1234@gmail.com>
2025-03-13 16:09:28 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react-swc/wasm";
+
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
