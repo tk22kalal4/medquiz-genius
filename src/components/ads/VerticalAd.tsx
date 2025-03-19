df8e52e6e651d4c3e8d3e81e222947c229093234
tk22kalal4 <tarangkalal1234@gmail.com>
2025-03-19 13:18:26 +0530

    Create VerticalAd.tsx

---
+++ src/components/ads/VerticalAd.tsx
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import { GoogleAdSense } from './GoogleAdSense';
+
+export const VerticalAd: React.FC<{ className?: string }> = ({ className }) => {
+  return (
+    <GoogleAdSense
+      client="ca-pub-5920367457745298"
+      slot="7689829898"
+      className={`my-4 ${className || ''}`}
+      style={{ display: 'block', minHeight: '280px' }}
+    />
+  );
+};
