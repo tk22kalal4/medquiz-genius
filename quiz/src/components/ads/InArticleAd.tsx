4ed168dc73e1ea3e6f1817e55e6af488ac6c596d
tk22kalal4 <tarangkalal1234@gmail.com>
2025-03-20 19:59:27 +0530

    Update InArticleAd.tsx

--- quiz/src/components/ads/InArticleAd.tsx
+++ quiz/src/components/ads/InArticleAd.tsx
@@ -6,8 +6,9 @@ export const InArticleAd: React.FC<{ className?: string }> = ({ className }) => 
   return (
     <GoogleAdSense
       client="ca-pub-5920367457745298"
-      slot="5678912345" // Replace with your actual In-article ad slot
+      slot="4319901842"
       format="fluid"
+      layout="in-article"
       responsive={true}
       className={`my-4 ${className || ''}`}
       style={{ display: 'block', textAlign: 'center' }}
