36aa19fa6c003a9687023cfabd631040272810ef
tk22kalal4 <tarangkalal1234@gmail.com>
2025-03-20 20:05:05 +0530

    Update VerticalAd.tsx

--- src/components/ads/VerticalAd.tsx
+++ src/components/ads/VerticalAd.tsx
@@ -7,6 +7,8 @@ export const VerticalAd: React.FC<{ className?: string }> = ({ className }) => {
     <GoogleAdSense
       client="ca-pub-5920367457745298"
       slot="7689829898"
+      format="auto"
+      responsive={true}
       className={`my-4 ${className || ''}`}
       style={{ display: 'block', minHeight: '280px' }}
     />
