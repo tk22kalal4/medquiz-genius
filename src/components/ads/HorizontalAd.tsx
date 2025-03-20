5e4bab642cf3197121a89efafc8d33be23b60419
tk22kalal4 <tarangkalal1234@gmail.com>
2025-03-20 20:04:24 +0530

    Update HorizontalAd.tsx

--- src/components/ads/HorizontalAd.tsx
+++ src/components/ads/HorizontalAd.tsx
@@ -7,6 +7,8 @@ export const HorizontalAd: React.FC<{ className?: string }> = ({ className }) =>
     <GoogleAdSense
       client="ca-pub-5920367457745298"
       slot="6240477852"
+      format="auto"
+      responsive={true}
       className={`my-4 ${className || ''}`}
       style={{ display: 'block', minHeight: '90px' }}
     />
